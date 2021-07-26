a54970dfea42075cd423bc7bafc760f29ea1c152
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2021-07-25 21:30:17 -0300

    💡: Add redux store devtools info comment

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,4 +8,6 @@ export default configureStore({
 	reducer: {
 		updateUI: updateUIReducer,
 	},
+	// In production devTools will be disabled
+	// devTools: process.env.NODE_ENV === 'development' ? true : false,
 })
