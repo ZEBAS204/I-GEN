f5e8a0707ad1cd0cd9b96cd9c436234f998b7acd
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-03-11 20:56:42 -0300

    Update context

--- src/layouts/AppContext.js
+++ src/layouts/AppContext.js
@@ -6,15 +6,21 @@ const AppContext = createContext()
 const AppContextProvider = ({ children }) => {
 	// the value that will be given to the context
 	const [isSettingVisible, setSettingVisible] = useState(false)
+	const [isTimerVisible, setTimerVisible] = useState(true)
 	const [time, setTime] = useState(10000)
 	const [isRunning, setRunning] = useState(false)
 	const [reset, setReset] = useState(false)
+	const [gen, sendGenerate] = useState(false)
 
 	// memoize the full context value
 	const contextValue = useMemo(
 		() => ({
 			isSettingVisible,
 			toggleSettingVisible: () => setSettingVisible((e) => !e),
+			gen,
+			generateWord: () => sendGenerate((e) => !e),
+			isTimerVisible,
+			toggleTimerVisible: () => setTimerVisible((e) => !e),
 			time,
 			changeTime: () => setTime((e) => e),
 			isRunning,
@@ -25,6 +31,10 @@ const AppContextProvider = ({ children }) => {
 		[
 			isSettingVisible,
 			setSettingVisible,
+			gen,
+			sendGenerate,
+			isTimerVisible,
+			setTimerVisible,
 			time,
 			isRunning,
 			setTime,
@@ -44,6 +54,10 @@ const AppContextProvider = ({ children }) => {
 /**
  * @param {boolean} isSettingVisible
  * @param {function} toggleSettingVisible
+ * @param {function} gengenerateWord
+ * @param {boolean} isTimerVisible
+ * @param {boolean} isTimerVisible
+ * @param {function} toggleTimerVisible
  * @param {number} time Time for countdown
  * @param {boolean} isRunning Set if time is running
  * @param {function} setTime
