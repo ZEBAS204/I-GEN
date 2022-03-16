901f4d1a6e5e0da84f842ea8ec9a40f394b7dacf
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-03-16 19:59:38 -0300

    Revert fast Math.floor

--- src/components/WordGenerator.js
+++ src/components/WordGenerator.js
@@ -107,7 +107,7 @@ export default function WordGenerator({ disableTTS }) {
  */
 function shuffleArray(array) {
 	for (let i = array.length - 1; i > 0; i--) {
-		const j = ~~(Math.random() * (i + 1)) // ~~ is the same as Math.floor but faster
+		const j = Math.floor(Math.random() * (i + 1))
 		;[array[i], array[j]] = [array[j], array[i]]
 	}
 	return array[0]
