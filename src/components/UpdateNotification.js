c968048984295a40580cc007b710a7563b1474f3
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-03-24 18:32:07 -0300

    Improve update notification

--- src/components/UpdateNotification.js
+++ src/components/UpdateNotification.js
@@ -1,35 +1,27 @@
 import {
-	useMediaQuery,
 	Button,
-	Tag,
-	Icon,
-	HStack,
 	Flex,
 	Spacer,
-	Heading,
+	Alert,
+	AlertIcon,
+	AlertTitle,
 } from '@chakra-ui/react'
-import { RiRefreshLine } from 'react-icons/ri'
+import { FiRefreshCcw } from 'react-icons/fi'
 
 export default function ServiceWorkerUpdate({ registration }) {
-	// Update responsive
-	const [isInMobileView] = useMediaQuery('(max-width: 501px)')
-	const size = isInMobileView ? 'xs' : 'sm'
-
 	const reloadPage = () => {
 		registration?.postMessage({ type: 'SKIP_WAITING' })
 		window.location.reload(true)
 	}
 
 	return (
-		<Tag as={Flex} display="flex" variant="solid" p={2} borderRadius={0}>
-			<HStack spacing={3}>
-				<Icon as={RiRefreshLine} w={5} h={5} />
-				<Heading size={size}>A new version is available!</Heading>
-			</HStack>
+		<Alert as={Flex} status="info">
+			<AlertIcon as={FiRefreshCcw} />
+			<AlertTitle>A new version is available!</AlertTitle>
 			<Spacer />
-			<Button variant="solid" size={size} ml={1} onClick={reloadPage}>
+			<Button variant="solid" size="sm" ml={1} onClick={reloadPage}>
 				Refresh
 			</Button>
-		</Tag>
+		</Alert>
 	)
 }
