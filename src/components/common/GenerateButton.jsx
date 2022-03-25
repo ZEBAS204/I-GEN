948328b3b6b8ec4887127c6959d53c8ac4f05134
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-03-24 23:44:32 -0300

    Prettier format jsx

--- src/components/common/GenerateButton.jsx
+++ src/components/common/GenerateButton.jsx
@@ -1,28 +1,24 @@
-import {useRef} from 'react'
+import { useRef } from 'react'
 import { Button } from '@chakra-ui/react'
 import { useKeyPressEvent } from 'react-use'
 
 import { useTranslation } from 'react-i18next'
 import { useAppContext } from '../../layouts/AppContext'
 
-
 export default function GenerateButton() {
 	const { t } = useTranslation()
-  const { generateWord } = useAppContext()
+	const { generateWord } = useAppContext()
 
-  // Bind SPACE to generator
-  const genButton = useRef(null)
-  useKeyPressEvent('space',
-    genButton.current?.click()
-  )
+	// Bind SPACE to generator
+	const genButton = useRef(null)
+	useKeyPressEvent('space', genButton.current?.click())
 
-  return (
-  <Button
-    variant="solid"
-    alignSelf="center"
-    marginY={5}
-    onClick={generateWord}
-    children={<h1>{t('buttons.regenerate')}</h1>}
-  />
-  )
+	return (
+		<Button
+			variant="solid"
+			alignSelf="center"
+			onClick={generateWord}
+			children={<h1>{t('buttons.regenerate')}</h1>}
+		/>
+	)
 }
