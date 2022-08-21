29bfc8d5441c18fa7e6bf19c30be44067a0435c3
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-08-21 20:29:24 -0300

    Update theme editor

--- src/components/settings/CustomThemeEditor.jsx
+++ src/components/settings/CustomThemeEditor.jsx
@@ -1,64 +1,21 @@
-import { useState, useCallback } from 'react'
-import { ButtonGroup, Button } from '@chakra-ui/react'
-
 import {
 	ThemeEditor,
+	ThemeEditorButton,
 	ThemeEditorDrawer,
 	ThemeEditorColors,
-	useThemeEditor,
+	ThemeEditorFontSizes,
 } from '@hypertheme-editor/chakra-ui'
-
 import { CgColorPicker } from 'react-icons/cg'
+import { RiFontSize2 } from 'react-icons/ri'
 
-const CustomThemeEditor = () => {
-	/**
-	 * Allows to manage the current theme
-	 * @see https://www.hyperthe.me/documentation/hooks/useThemeEditor
-	 */
-	const [isOpen, setIsOpen] = useState(false)
-	const { initialTheme, theme, setTheme, canUndo, undo, canRedo, redo } =
-		useThemeEditor()
-
-	const handleReset = () => setTheme(initialTheme)
-
-	const handleColorChange = useCallback(() => {
-		setTheme({
-			...theme,
-			colors: {
-				blue: {
-					...theme.colors.blue,
-				},
-			},
-		})
-	}, [theme])
-
-	return (
-		<>
-			<Button
-				onClick={() => {
-					setIsOpen(!isOpen)
-				}}
-			>
-				Open me
-			</Button>
-			<ThemeEditor isOpen={isOpen}>
-				<ThemeEditorDrawer hideUpgradeToPro hideCredits>
-					<ThemeEditorColors icon={CgColorPicker} title="Colors" />
-				</ThemeEditorDrawer>
-			</ThemeEditor>
-			<ButtonGroup variant="outline" spacing="6">
-				<Button disabled={!canUndo} onClick={undo}>
-					Undo
-				</Button>
-				<Button disabled={!canRedo} onClick={redo}>
-					Redo
-				</Button>
-				<Button disabled={!canUndo} onClick={handleReset}>
-					Reset Theme
-				</Button>
-			</ButtonGroup>
-		</>
-	)
-}
+const CustomThemeEditor = (props) => (
+	<ThemeEditor>
+		<ThemeEditorButton size="sm" {...props} />
+		<ThemeEditorDrawer hideUpgradeToPro>
+			<ThemeEditorColors icon={CgColorPicker} title="Colors" />
+			<ThemeEditorFontSizes icon={RiFontSize2} title="Font Sizes" />
+		</ThemeEditorDrawer>
+	</ThemeEditor>
+)
 
 export default CustomThemeEditor
