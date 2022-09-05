86a261e9065f1416f32624fe0828dcf529944132
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-09-05 19:42:51 -0300

    Update global context

--- src/layouts/AppContext.jsx
+++ src/layouts/AppContext.jsx
@@ -1,109 +1,25 @@
-import {
-	createContext,
-	useState,
-	useContext,
-	useMemo,
-	useCallback,
-} from 'react'
-import { useMediaQuery } from '@chakra-ui/react'
+import { createContext, useState, useContext, useMemo } from 'react'
+import { useLocalForage } from '@utils/appStorage'
 
-import { mobileViewMQ } from '../utils/constants'
-import { useLocalForage } from '../utils/appStorage'
-
-// Constants
-const DEF_TIME = 600 // 10 min
-const MIN_TIME = 0
-const MAX_TIME = 359999 // 99h 59m 59s
-
-// create context
+// Create context
 const AppContext = createContext()
 
 const AppContextProvider = ({ children }) => {
 	// the value that will be given to the context
-	const [isInMobileView] = useMediaQuery(mobileViewMQ)
-	const [isSettingVisible, setSettingVisible] = useState(false)
-	const [isTimePickerVisible, setTimePickerVisible] = useState(false)
-	const [isTimerVisible, setTimerVisible] = useLocalForage(
-		'timer_visible',
-		false
-	)
 	const [speak, setSpeak] = useLocalForage('tts_enabled', false)
-	const [time, setTime] = useLocalForage('countdown_time', DEF_TIME)
-	const [isRunning, setRunning] = useState(false)
-	const [reset, setReset] = useState(false)
 	const [gen, sendGenerate] = useState(false)
-
-	const stopTimer = useCallback(
-		() => isRunning && setRunning(false),
-		[isRunning]
-	)
 
 	// memoize the full context value
 	const contextValue = useMemo(
 		() => ({
-			isInMobileView,
-			isSettingVisible,
-			toggleSettingVisible: () => {
-				stopTimer()
-				setSettingVisible((e) => !e)
-			},
-			isTimePickerVisible,
-			toggleTimePickerVisible: () => {
-				stopTimer()
-				setTimePickerVisible((e) => !e)
-			},
 			gen,
-			generateWord: () =>
-				!isSettingVisible && !isTimePickerVisible && sendGenerate((e) => !e),
-			isTimerVisible,
-			toggleTimerVisible: () => {
-				stopTimer()
-				setTimerVisible(!isTimerVisible)
-			},
-			time,
-			changeTime: (e) => checkTimeValue(e) && setTime(e),
+			generateWord: () => sendGenerate((e) => !e),
+
 			speak,
 			toggleSpeak: (e) => (e ? setSpeak(e) : setSpeak(!speak)),
-			isRunning,
-			toggleRunning: () => setRunning((e) => !e),
-			reset,
-			sendReset: () => setReset((e) => !e),
 		}),
-		[
-			isInMobileView,
-			isSettingVisible,
-			setSettingVisible,
-			isTimePickerVisible,
-			setTimePickerVisible,
-			gen,
-			sendGenerate,
-			isTimerVisible,
-			setTimerVisible,
-			speak,
-			setSpeak,
-			time,
-			setTime,
-			isRunning,
-			setRunning,
-			reset,
-			setReset,
-			stopTimer,
-		]
+		[gen, sendGenerate, speak, setSpeak]
 	)
-
-	/*
-	console.log('%cCONTEXT UPDATED', 'color: #00ff00')
-	console.table({
-		IS_IN_MOBILE_VIEW: [isInMobileView],
-		SETTINGS_VISIBLE: [isSettingVisible],
-		TIMER_VISIBLE: [isTimerVisible],
-		TIMER_TIMER: [time],
-		TIMER_RUNNING: [isRunning],
-		TIMER_RESET: [reset],
-		WORD_GENERATOR: [gen],
-		TTS_ENABLED: [speak],
-	})
-	*/
 
 	return (
 		// the Provider gives access to the context to its children
@@ -113,18 +29,7 @@ const AppContextProvider = ({ children }) => {
 
 // context consumer hook
 /**
- * @param {boolean} isSettingVisible
- * @param {function} toggleSettingVisible
  * @param {function} gengenerateWord
- * @param {boolean} isTimerVisible
- * @param {boolean} isTimerVisible
- * @param {function} toggleTimerVisible
- * @param {number} time Time for countdown
- * @param {boolean} isRunning Set if time is running
- * @param {function} setTime
- * @param {function} setRunning
- * @param {boolean} reset
- * @param {function} sendReset
  * @param {boolean} speak
  * @returns {React.context<AppContext>}
  */
@@ -141,16 +46,3 @@ const useAppContext = () => {
 }
 
 export { AppContextProvider, AppContext, useAppContext }
-
-/**
- * Checks if the passed time is valid for this component
- * @param {Number} time
- * @return {Boolean}
- */
-const checkTimeValue = (time) => {
-	// Check if stored state is invalid
-	if (!time || typeof time !== 'number' || time < MIN_TIME || time > MAX_TIME)
-		return false
-
-	return true
-}
