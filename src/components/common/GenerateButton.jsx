a5e8f8cbe836d55e008bcd729bcb59480f72bd3c
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-09-05 18:37:01 -0300

    Prevent unnecessary re-renders of button
    Use the wrap Context function to memoize button component and prevent
    triggering a re-render when the context changes.

--- src/components/common/GenerateButton.jsx
+++ src/components/common/GenerateButton.jsx
@@ -1,25 +1,56 @@
+import { memo, useCallback } from 'react'
 import { Button } from '@chakra-ui/react'
 import { useKeyPressEvent } from 'react-use'
 
 import { useTranslation } from 'react-i18next'
-import { useAppContext } from '../../layouts/AppContext'
+import { useAppContext } from '@layouts/AppContext'
+import { wrapContext } from '@contexts/contextWrapper'
 
-export default function GenerateButton() {
+//* Function that avoid re-renders from the context props
+const contextProps = () => {
+	// Memoize callback and prevent re-renders of the button when not needed
+	const { generateWord } = useCallback(useAppContext(), [])
+
+	return {
+		generateWord,
+	}
+}
+
+const GenerateButton = memo(({ generateWord }) => {
 	const { t } = useTranslation()
-	const { generateWord } = useAppContext()
 
 	// Bind SPACE to generator (yes, the selector is a space...)
-	useKeyPressEvent(
-		' ',
-		(event) => event.target === document.body && generateWord()
-	)
+	useKeyPressEvent(' ', (event) => {
+		if (event.target === document.body) {
+			event.preventDefault() // prevent scroll
+			generateWord()
+		}
+	})
 
 	return (
 		<Button
-			variant="solid"
-			alignSelf="center"
+			aria-label="Generate new word set"
+			aria-keyshortcuts="Space"
 			onClick={generateWord}
-			children={<h1>{t('buttons.regenerate')}</h1>}
-		/>
+			fontFamily="Poppins"
+			fontWeight="700"
+			fontSize="2xl"
+			w={['22%', '20%']}
+			p={8}
+			colorScheme="purple"
+			bg="#8969eb"
+			_hover={{
+				bg: '#6e4fcf',
+			}}
+			_active={{
+				bg: '#6e4fcf',
+			}}
+			color="#f0f0f0"
+			clipPath="polygon(0 20px, 20px 0, calc(100% - 20px) 0, 100% 20px, 100% 100%, 100% 100%, 0 100%)"
+		>
+			{t('buttons.regenerate')}
+		</Button>
 	)
-}
+})
+
+export default wrapContext(GenerateButton, contextProps)
