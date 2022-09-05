eb5ca353af00b4d6658c7ff58c589dbba7548d1b
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-09-05 19:57:57 -0300

    Update interface

--- src/components/settings/Interface.jsx
+++ src/components/settings/Interface.jsx
@@ -1,20 +1,44 @@
-import { Box, Text, Link, Icon, Heading, Select } from '@chakra-ui/react'
+import {
+	Box,
+	Text,
+	Link,
+	Icon,
+	Heading,
+	Stack,
+	Select as ChakraSelect,
+	Spacer,
+} from '@chakra-ui/react'
 import { RiExternalLinkLine } from 'react-icons/ri'
-
 import { useTranslation } from 'react-i18next'
-import { supportedLanguages } from '../../utils/supportedLanguages'
+
+import { useLocalForage } from '@utils/appStorage'
+import {
+	supportedLanguages,
+	supportedWordsLanguages,
+} from '@utils/supportedLanguages'
+
+const Select = (props) => (
+	<Box shadow="base" borderRadius="md">
+		<ChakraSelect variant="filled" {...props}>
+			{props.children}
+		</ChakraSelect>
+	</Box>
+)
 
 export default function Interface() {
 	const { t, i18n } = useTranslation()
 
+	const [wordsLang, setWordsLang] = useLocalForage('settings.words_lang')
+	const [nounLang, setNounLang] = useLocalForage('settings.lang_noun')
+	const [adjLang, setAdjLang] = useLocalForage('settings.lang_adj')
+
 	return (
 		<>
-			<Heading size="md">{t('settings.language')}</Heading>
-			<br />
-			<Heading size="sm">Select language to use</Heading>
-			<Box shadow="base" borderRadius="md" marginTop={2}>
+			<Stack direction="row" alignItems="center">
+				<Heading size="md">{t('settings.language')}</Heading>
+				<Spacer />
+
 				<Select
-					variant="filled"
 					value={
 						// Get current language without country code
 						i18n.languages[0]
@@ -30,16 +54,75 @@ export default function Interface() {
 						))
 					}
 				</Select>
-			</Box>
-			<br />
-			<Text>
-				If you would like to contribute to translations
-				{
-					// TODO: Use environment variable
-					' '
-				}
+			</Stack>
+			<Stack direction="row" alignItems="center" mt={4}>
+				<Heading size="md">{t('settings.language_words')}</Heading>
+				<Spacer />
+				<Select
+					value={
+						// Get current language without country code
+						wordsLang
+					}
+					onChange={(e) => setWordsLang(e.target.value)}
+				>
+					{
+						// Get all available languages
+						supportedWordsLanguages.map((lang, key) => (
+							<option value={lang.code} key={`wordlng-${lang}-${key}`}>
+								{t(`languages.${lang.code}`)}
+							</option>
+						))
+					}
+					<option value="custom">{t('languages.custom')}</option>
+				</Select>
+			</Stack>
+
+			{
+				// Allow to manually set language options when language="custom"
+				wordsLang === 'custom' && (
+					<Stack direction="row" mt={2}>
+						<Stack direction="column" alignItems="center">
+							<Heading size="sm" color="whiteAlpha.800">
+								{t('common.noun')}
+							</Heading>
+							<Select
+								variant="filled"
+								value={nounLang}
+								onChange={(e) => setNounLang(e.target.value)}
+							>
+								{supportedWordsLanguages.map((lang, key) => (
+									<option value={lang.code} key={`wordlng-${lang}-${key}`}>
+										{t(`languages.${lang.code}`)}
+									</option>
+								))}
+							</Select>
+						</Stack>
+						<Spacer />
+						<Stack direction="column" alignItems="center">
+							<Heading size="sm" color="whiteAlpha.800">
+								{t('common.adjective')}
+							</Heading>
+							<Select
+								variant="filled"
+								value={adjLang}
+								onChange={(e) => setAdjLang(e.target.value)}
+							>
+								{
+									// Get all available languages
+									supportedWordsLanguages.map((lang, key) => (
+										<option value={lang.code} key={`wordlng-${lang}-${key}`}>
+											{t(`languages.${lang.code}`)}
+										</option>
+									))
+								}
+							</Select>
+						</Stack>
+					</Stack>
+				)
+			}
+			<Text pt={2}>
 				<Link href="#" isExternal>
-					click here <Icon as={RiExternalLinkLine} />
+					{t('settings.language_contribute')} <Icon as={RiExternalLinkLine} />
 				</Link>
 			</Text>
 		</>
