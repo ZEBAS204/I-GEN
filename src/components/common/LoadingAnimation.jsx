dff244b874b55761a09a4c449443c91e07588d97
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-09-05 19:32:30 -0300

    Add loading animation component

---
+++ src/components/common/LoadingAnimation.jsx
@@ -0,0 +1,42 @@
+/*
+ * Generated using cssload.net
+ */
+import PropTypes from 'prop-types'
+import { useTranslation } from 'react-i18next'
+import { Box, Text } from '@chakra-ui/react'
+import '@styles/components/LoadingAnimation.scss'
+
+export const LoadingAnimation = (props) => (
+	<Box className="cssload-coffee" {...props} />
+)
+
+export const LoadingAnimationContainer = (props) => {
+	const { t } = useTranslation()
+	const { showLabel, ...animationProps } = props
+
+	return (
+		<Box
+			w="100%"
+			h="100%"
+			display="flex"
+			flexDir="column"
+			alignItems="center"
+			justifyContent="center"
+		>
+			<LoadingAnimation {...animationProps} />
+			{showLabel && (
+				<Text position="relative" top="7em">
+					{t('common.loading')}...
+				</Text>
+			)}
+		</Box>
+	)
+}
+
+LoadingAnimationContainer.propTypes = {
+	showLabel: PropTypes.bool,
+}
+
+LoadingAnimationContainer.defaultProps = {
+	showLabel: true,
+}
