9b5745b97347ff22a63924618c26ead1daad29cc
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2022-09-05 19:27:34 -0300

    Remove settings animations

--- src/components/settings/index.jsx
+++ src/components/settings/index.jsx
@@ -1,33 +1,5 @@
-import { AnimatePresence, motion } from 'framer-motion'
-import { useAppContext } from '../../layouts/AppContext'
 import SettingsPage from './Settings'
 
-const animationVariants = {
-	hidden: { y: -20, opacity: 0 },
-	visible: { y: 0, opacity: 1 },
-	exit: { y: -20, opacity: 0 },
-}
-
 export default function Settings() {
-	const { isSettingVisible } = useAppContext()
-
-	return (
-		<AnimatePresence>
-			{isSettingVisible && (
-				<>
-					<motion.div
-						key="settings-motion"
-						id="animation-container"
-						variants={animationVariants}
-						initial="hidden"
-						animate="visible"
-						exit="exit"
-					>
-						<SettingsPage />
-					</motion.div>
-					<div id="blur-background" />
-				</>
-			)}
-		</AnimatePresence>
-	)
+	return <SettingsPage />
 }
