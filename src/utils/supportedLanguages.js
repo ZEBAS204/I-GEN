cad45dfaac6ccbf8ac540c1066ba98122980961b
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-02-07 20:26:51 -0300

    Add new wordsets
    Updated:
     - english
    
    Added:
    - Arabic
    - Chinese
    - Czech
    - Danish
    - Dutch
    - Finnish
    - French
    - German
    - Hindi
    - Hungarian
    - Italian
    - Japanese
    - Korean
    - Polish
    - Portuguese
    - Russian
    - Spanish
    - Swedish
    - Turkish

--- src/utils/supportedLanguages.js
+++ src/utils/supportedLanguages.js
@@ -4,4 +4,25 @@ export const supportedLanguages = [
 	{ code: 'fr', name: 'Français' },
 ]
 
-export const supportedWordsLanguages = [{ code: 'en', name: 'English' }]
+export const supportedWordsLanguages = [
+	{ code: 'ar', name: 'Arabic', flip: true },
+	{ code: 'cs', name: 'Czech' },
+	{ code: 'da', name: 'Danish' },
+	{ code: 'de', name: 'German' },
+	{ code: 'en', name: 'English' },
+	{ code: 'es', name: 'Spanish', flip: true },
+	{ code: 'fi', name: 'Finnish' },
+	{ code: 'fr', name: 'French', flip: true },
+	{ code: 'hi', name: 'Hindi' },
+	{ code: 'hu', name: 'Hungarian' },
+	{ code: 'it', name: 'Italian', flip: true },
+	{ code: 'ja', name: 'Japanese' },
+	{ code: 'ko', name: 'Korean' },
+	{ code: 'nl', name: 'Dutch' },
+	{ code: 'pl', name: 'Polish' },
+	{ code: 'pt', name: 'Portuguese', flip: true },
+	{ code: 'ru', name: 'Russian' },
+	{ code: 'sv', name: 'Swedish' },
+	{ code: 'tr', name: 'Turkish' },
+	{ code: 'zh', name: 'Chinese' },
+]
