04519644573557dfeae5308163c75f13fe8d87dc
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-02-28 20:17:18 -0300

    Fix undefined global variables in tests

--- vitest.config.js
+++ vitest.config.js
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 // Relative alias
 import { alias } from './vite-alias.config'
 
+const empty = () => '""'
+
 export default defineConfig({
 	plugins: [react()],
+	define: {
+		__GIT_REVISION__: empty(),
+		__GIT_BRANCH__: empty(),
+		__PROJECT_SOURCE__: empty(),
+		__CONTRIBUTE_TRANSLATION__: empty(),
+	},
 	test: {
 		globals: true,
 		environment: 'jsdom',
