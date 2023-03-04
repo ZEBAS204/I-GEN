e928cff85e7c8afc1f32d794c41dd5cdd4be9518
Sebas <36385457+ZEBAS204@users.noreply.github.com>
2023-03-04 20:41:54 -0300

    Add Clock test

---
+++ src/__tests__/components/timer/Clock..test.jsx
@@ -0,0 +1,28 @@
+import { describe, test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import { ChakraProvider } from '@chakra-ui/react'
+import Clock from '@components/timer/Clock'
+
+describe('Content Error', () => {
+	test('Should render', () => {
+		const time = '42:06:09'
+		const time2 = '59:59:59'
+
+		const { getByText, rerender } = render(
+			<ChakraProvider>
+				<Clock remainingtimeToDisplay={time} />
+			</ChakraProvider>
+		)
+
+		expect(getByText(time)).toBeDefined()
+
+		rerender(
+			<ChakraProvider>
+				<Clock remainingtimeToDisplay={time2} />
+			</ChakraProvider>
+		)
+
+		expect(screen.getByText(time2)).toBeDefined()
+	})
+})
